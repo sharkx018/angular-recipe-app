38b743fd15206e4fd60da3cb0d48c1538bf32c8f
sharkx018 <aryanv018@gmail.com>
2023-11-12 15:41:52 +0530

    Add ingredients to the recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,18 +1,31 @@
 import {Recipe} from "./recipe.model";
 import {EventEmitter} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
 
 export class RecipeService{
 
     private recipes : Recipe[] = [
         new Recipe('A Test Recipe 1',
             'This is simply a test',
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJrXnVVt766jPFKXdsUeZGeoC5_JqAOHLQSw&usqp=CAU'),
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJrXnVVt766jPFKXdsUeZGeoC5_JqAOHLQSw&usqp=CAU',
+            [
+                new Ingredient("Meat", 2),
+                new Ingredient("Bread", 1)
+            ]),
         new Recipe('A Test Recipe 2',
             'This is simply a test',
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJrXnVVt766jPFKXdsUeZGeoC5_JqAOHLQSw&usqp=CAU'),
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJrXnVVt766jPFKXdsUeZGeoC5_JqAOHLQSw&usqp=CAU',
+            [
+                new Ingredient("Apple", 1),
+                new Ingredient("Honey", 3)
+            ]),
         new Recipe('A Test Recipe 3',
             'This is simply a test',
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJrXnVVt766jPFKXdsUeZGeoC5_JqAOHLQSw&usqp=CAU')
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJrXnVVt766jPFKXdsUeZGeoC5_JqAOHLQSw&usqp=CAU',
+            [
+                new Ingredient("Tea", 1),
+                new Ingredient("Bread", 5)
+            ])
     ];
 
     recipeSelected = new EventEmitter<Recipe>()
