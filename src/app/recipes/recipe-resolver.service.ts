672cac525ff0f972a68d074aac7f0868708453a6
sharkx018 <aryanv018@gmail.com>
2023-11-23 19:50:30 +0530

    Added the navigation for non exist recipe detail component

--- src/app/recipes/recipe-resolver.service.ts
+++ src/app/recipes/recipe-resolver.service.ts
@@ -1,20 +1,40 @@
-import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from "@angular/router";
+import {ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot} from "@angular/router";
 import {Recipe} from "./recipe.model";
-import {Observable} from "rxjs";
+import {Observable, tap} from "rxjs";
 // @ts-ignore
 import Promise from "$GLOBAL$";
 import {DataStorageService} from "../shared/data-storage.service";
 import {Injectable} from "@angular/core";
+import {RecipeService} from "./recipe.service";
 
 
 @Injectable({providedIn:'root'})
 export class RecipeResolverService implements Resolve<Recipe[]>{
-    constructor(private dataStorageService: DataStorageService) {}
+    constructor(private dataStorageService: DataStorageService,
+                private router: Router,
+                private recipeService: RecipeService) {}
 
     resolve(route: ActivatedRouteSnapshot,
-            state: RouterStateSnapshot) {
+            state: RouterStateSnapshot,) {
+
+        const recipes = this.recipeService.getRecipes()
+        if (recipes.length != 0){
+            return recipes
+        }
+
 
         return this.dataStorageService.fetchRecipes()
+            .pipe(
+                tap((recipes)=>{
+                    const isFound = recipes.findIndex((recipe)=>{
+                        return recipe.id === +route.params['id']
+                    })
+
+                    if (isFound == -1) {
+                        this.router.navigate(['recipes'])
+                    }
+                })
+            )
 
     }
 
