91c2cd635c07921f747291a66972916729bcf923
sharkx018 <aryanv018@gmail.com>
2023-11-24 11:49:24 +0530

    Creating the separate recipe module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,24 +20,20 @@ import { RecipeService } from "./recipes/recipe.service";
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {AuthComponent} from "./auth/auth.component";
 import {LoadingSpinnerComponent} from "./shared/loading-spinner/loading-spinner.component";
-import {AuthService} from "./auth/auth.service";
 import {AuthInterceptorService} from "./auth/auth-interceptor.service";
+import {AlertComponent} from "./shared/alert/alert.component";
+import {RecipeModule} from "./recipes/recipe.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
     AuthComponent,
-    LoadingSpinnerComponent
+    LoadingSpinnerComponent,
+    AlertComponent
   ],
     imports: [
         BrowserModule,
@@ -45,7 +41,8 @@ import {AuthInterceptorService} from "./auth/auth-interceptor.service";
         AppRoutingModule,
         FormsModule,
         HttpClientModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        RecipeModule
     ],
   providers: [
       ShoppingListService,
