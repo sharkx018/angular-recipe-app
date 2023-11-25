3544a4218d62a5418aac558b8d77cfd6a35227d0
sharkx018 <aryanv018@gmail.com>
2023-11-25 22:55:02 +0530

    updated the env variable

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    FIREBASE_API_KEY : '${FIREBASE_API_KEY}',
-    BASE_RECIPE_URL : '${BASE_RECIPE_URL}'
+    FIREBASE_API_KEY : `$FIREBASE_API_KEY`,
+    BASE_RECIPE_URL : `$BASE_RECIPE_URL`
 }
