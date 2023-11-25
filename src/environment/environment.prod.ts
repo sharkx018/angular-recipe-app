3b1b66eda92eb43e1ff52d8e8dd0506e038b21b7
sharkx018 <aryanv018@gmail.com>
2023-11-25 18:01:18 +0530

    setup workflow for deployments

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    FIREBASE_API_KEY : process.env.firebaseApiKey,
+    BASE_RECIPE_URL : process.env.baseRecipeUrl,
+}
---
+++ src/environment/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    FIREBASE_API_KEY : process.env.firebaseApiKey,
+    BASE_RECIPE_URL : process.env.baseRecipeUrl,
+}
