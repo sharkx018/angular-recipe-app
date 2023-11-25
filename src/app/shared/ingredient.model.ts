aa58bc1da38867048219b5c5f9a92da7cd1f2f6b
sharkx018 <aryanv018@gmail.com>
2023-11-25 23:44:25 +0530

    checking the github actions

--- src/app/shared/ingredient.model.ts
+++ src/app/shared/ingredient.model.ts
@@ -2,6 +2,8 @@ export class Ingredient{
     public name: string
     public amount: number
 
+
+    
     constructor(name: string, amount: number) {
         this.name = name;
         this.amount = amount
